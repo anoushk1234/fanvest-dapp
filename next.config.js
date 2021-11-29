a6133b7615ef0dd5f15c6b13e724553c2bfaa505
anoushk1234 <anoushka1828@gmail.com>
2021-11-29 19:49:32 +0530

    elctron ignore

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@
 // const withVideos = require("next-videos");
 module.exports = {
   reactStrictMode: false,
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^electron$/,
+      })
+    );
+    return config;
+  },
 };
