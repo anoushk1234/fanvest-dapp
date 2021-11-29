ea48ca22f635976b04e4880ce3e6590b6ed45eee
anoushk1234 <anoushka1828@gmail.com>
2021-11-29 20:21:24 +0530

    future

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 // const withVideos = require("next-videos");
 module.exports = {
   reactStrictMode: false,
+  future: { webpack5: true },
   webpack: (config, { webpack }) => {
     config.plugins.push(
       new webpack.IgnorePlugin({
